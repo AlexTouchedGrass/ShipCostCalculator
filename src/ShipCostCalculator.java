e863375b94303da18bc621f962456209e1e9c56a
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-09-27 09:20:42 -0400

    Part 1 of Lab 5 Unit 5

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,5 +1,22 @@
-public class Main {
+import java.util.Scanner;
+
+public class ShipCostCalculator {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        double itemCost = 0;
+
+        System.out.println("Please enter the price of your item. If 100$ or greater 2% shipping tax's will apply.");
+        itemCost = scan.nextInt();
+
+        if(itemCost < 100 && itemCost > 0){
+            itemCost = itemCost * 1.02;
+            System.out.println("Your item costs $" + itemCost + ". Shipping costs were applied.");
+
+        }
+        else {
+            System.out.println("Your item costs $" + itemCost + ". Shipping is free!");
+
+        }
     }
 }
